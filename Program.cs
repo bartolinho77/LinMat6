fc19338a6f3567907e903c67661a47d5d0c64a77
Bartosz Paszkiewicz <bartosz.paszkiewicz@digitalworkforce.pl>
2020-01-26 09:37:11 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace LinMat6
+{
+    class Program
+    {
+        static void Main()
+        {
+            RecursiveDescentPredictiveParser parser = new RecursiveDescentPredictiveParser();
+            Console.WriteLine("Enter expression:");
+            string expression = Console.ReadLine();
+            Console.WriteLine("Result: " + parser.Parse(expression));
+        }
+    }
+}
