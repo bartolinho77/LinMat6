104e4ef2b42688c19b772cc79d5ceddab7b832db
Bartosz Paszkiewicz <bartosz.paszkiewicz@digitalworkforce.pl>
2020-02-02 18:29:25 +0100

    done

--- Program.cs
+++ Program.cs
@@ -4,16 +4,13 @@ using System.Collections.Generic;
 
 namespace LinMat6
 {
-
     public class GrammarChecker
     {
-
         private static readonly List<char> _firstW = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '(' };
         private static readonly List<char> _firstR = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         private static readonly List<char> _firstL = _firstR;
-        private static readonly List<char> _firstC = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private static readonly List<char> _firstC = _firstR;
         private static readonly List<char> _firstO = new List<char>() { '*', ':', '+', '-', '^' };
-
 
 
         private string _currentSentence;
@@ -25,12 +22,83 @@ namespace LinMat6
             _currentSentence = input;
             return CheckS();
         }
+        
+     
+        private bool CheckS()
+        {
+            return CheckW() && CheckCurrentSymbol(';') && CheckZ();
+        }
+
+        private bool CheckZ()
+        {
+            if (_firstW.Contains(GetCurrentSymbol()))
+                return CheckW() && CheckCurrentSymbol(';') && CheckZ();
+            else
+                return true;
+        }
+
+        private bool CheckW()
+        {
+            return CheckP() && CheckWprim();
+        }
+
+        private bool CheckWprim()
+        {
+            if (_firstO.Contains(GetCurrentSymbol()))
+                return CheckO() && CheckW();
+            else
+                return true;
+        }
+
+        private bool CheckP()
+        {
+            if (_firstR.Contains(GetCurrentSymbol()))
+                return CheckR();
+            
+            else
+                return CheckCurrentSymbol('(') && CheckW() && CheckCurrentSymbol(')');
+        }
+
+        private bool CheckR()
+        {
+            return CheckL() && CheckRprim();
+        }
+
+        private bool CheckRprim()
+        {
+            if (CheckCurrentSymbol('.'))
+                return CheckL();
+            else
+                return true;
+        }
+
+        private bool CheckL()
+        {
+            return CheckC() && CheckLprim();
+        }
+
+        private bool CheckLprim()
+        {
+            if (_firstL.Contains(GetCurrentSymbol()))
+                return CheckL();
+            else
+                return true;
+        }
+
+        private bool CheckC()
+        {
+            return CheckCurrentSymbol(_firstC);
+        }
+
+        private bool CheckO()
+        {
+            return CheckCurrentSymbol(_firstO);
+        }
         private bool CheckCurrentSymbol(char c)
         {
             if (IsItLastSymbol())
-            {
-                return false;
-            }
+                    return false;
+            
             if (c == GetCurrentSymbol())
             {
                 index++;
@@ -42,9 +110,8 @@ namespace LinMat6
         private bool CheckCurrentSymbol(List<char> symbols)
         {
             if (IsItLastSymbol())
-            {
                 return false;
-            }
+            
             if (symbols.Contains(GetCurrentSymbol()))
             {
                 index++;
@@ -63,98 +130,7 @@ namespace LinMat6
             return index >= _currentSentence.Length;
         }
 
-     
-        private bool CheckS()
-        {
-            return CheckW() && CheckCurrentSymbol(';') && CheckZ();
-        }
 
-        private bool CheckZ()
-        {
-            if (_firstW.Contains(GetCurrentSymbol()))
-            {
-                return CheckW() && CheckCurrentSymbol(';') && CheckZ();
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        private bool CheckW()
-        {
-            return CheckP() && CheckWprim();
-        }
-
-        private bool CheckWprim()
-        {
-            if (_firstO.Contains(GetCurrentSymbol()))
-            {
-                return CheckO() && CheckW();
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        private bool CheckP()
-        {
-            if (_firstR.Contains(GetCurrentSymbol()))
-            {
-                return CheckR();
-            }
-            else
-            {
-                return CheckCurrentSymbol('(') && CheckW() && CheckCurrentSymbol(')');
-            }
-        }
-
-        private bool CheckR()
-        {
-            return CheckL() && CheckRprim();
-        }
-
-        private bool CheckRprim()
-        {
-            if (CheckCurrentSymbol('.'))
-            {
-                return CheckL();
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        private bool CheckL()
-        {
-            return CheckC() && CheckLprim();
-        }
-
-        private bool CheckLprim()
-        {
-            if (_firstL.Contains(GetCurrentSymbol()))
-            {
-                return CheckL();
-            }
-            else
-            {
-                return true;
-            }
-        }
-
-        private bool CheckC()
-        {
-            return CheckCurrentSymbol(_firstC);
-        }
-
-        private bool CheckO()
-        {
-            return CheckCurrentSymbol(_firstO);
-        }
-
-        
     }
     class Program
     {
